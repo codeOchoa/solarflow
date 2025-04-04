a1a2b10647ebcb5a0ac7409286db6ac1fcb9e893
Pablo Matias Ochoa <158297956+codeOchoa@users.noreply.github.com>
2025-04-04 01:52:18 -0300

    Delete app/_zustand directory

--- app/_zustand/sortStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const useSortStore = create((set) => ({
-    sortBy: "defaultSort",
-    changeSortBy: (mode) => {
-        set(() => ({ sortBy: mode }));
-    },
-}));
